f48044cf70866dbd1169db30fa1695cbf8d83a61
sim-sal <simone.saladino93@gmail.com>
2023-06-05 18:56:01 +0200

    added header component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,33 @@ export default {
 </script>
 
 <template>
-    <header class="container">
-        <h1>{{ message }}</h1>
+    <header>
+
+        <div class="container">
+            <div id="header-title">
+                <img src="../assets/yu-gi-oh.png" alt="">
+                <h1>{{ message }}</h1>
+            </div>
+        </div>
+
     </header>
 </template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/_variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
+
+header .container {
+    height: 100px;
+
+    #header-title {
+        width: 350px;
+        height: 100%;
+        @include flex-between-center;
+
+        img {
+            width: 100px;
+        }
+    }
+}
+</style>

1a2accb45fb8becd377285295adfea8fcf35fa82
sim-sal <simone.saladino93@gmail.com>
2023-06-05 18:22:23 +0200

    installed all dependencies and set up structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: ['message'],
+}
+</script>
+
+<template>
+    <header class="container">
+        <h1>{{ message }}</h1>
+    </header>
+</template>

f159c02dd288a6f4517676fbfb312349cba3c15d
sim-sal <simone.saladino93@gmail.com>
2023-06-06 17:46:14 +0200

    added select in component

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@ import axios from 'axios';
 import AppHeader from './components/AppHeader.vue'
 import CharactersList from './components/CharactersList.vue'
 import AppLoader from './components/AppLoader.vue'
+import AppSelect from './components/AppSelect.vue'
 
 
 export default {
   components: {
     AppHeader,
     CharactersList,
-    AppLoader
+    AppLoader,
+    AppSelect
   },
   data() {
     return {
@@ -39,16 +41,7 @@ export default {
   <AppLoader v-if="store.loading" />
   <AppHeader message="Yu-Gi-Oh Api" />
   <main>
-    <div id="select-container">
-      <div>
-        <select name="" id="select-archetype">
-          <option value="">Select Archetype</option>
-          <option value=""></option>
-        </select>
-      </div>
-
-    </div>
-
+    <AppSelect />
     <CharactersList />
   </main>
 </template>
@@ -59,22 +52,5 @@ export default {
 main {
   background-color: rgb(212, 143, 58);
   padding: 0 100px 100px 100px;
-
-  #select-container {
-    max-width: max-content;
-    height: 100px;
-    margin: auto;
-    display: flex;
-    align-items: center;
-
-    #select-archetype {
-
-      border: none;
-      border-radius: 5px;
-      padding: 5px;
-    }
-  }
-
-
 }
 </style>
---
+++ src/components/AppSelect.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: "AppSelect",
+}
+</script>
+
+<template>
+    <div id="select-container">
+        <div>
+            <select name="" id="select-archetype">
+                <option value="">Select Archetype</option>
+                <option value=""></option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+#select-container {
+    max-width: max-content;
+    height: 100px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+
+    #select-archetype {
+
+        border: none;
+        border-radius: 5px;
+        padding: 5px;
+    }
+}
+</style>
